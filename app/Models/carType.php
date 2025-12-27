a3f7aff7d86a0be54eb5fe108e3952150cc9869b
nilani884 <nilani884@gmail.com>
2025-12-27 13:14:03 +0900

    more debug

--- app/Models/carType.php
+++ app/Models/carType.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class carType extends Model
+class  CarType extends Model
 {
     use HasFactory;
 }
